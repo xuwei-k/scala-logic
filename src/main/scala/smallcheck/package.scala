d5de4734e12974d98a07e74663356ae703f30d6d
xuwei-k <6b656e6a69@gmail.com>
2015-05-16 23:33:21 +0900

    generate

--- src/main/scala/smallcheck/package.scala
+++ src/main/scala/smallcheck/package.scala
@@ -1,5 +1,19 @@
+import logic.LogicT
+
+import scalaz.Kleisli
+
 package object smallcheck {
 
   type Depth = Int
+
+  def generate[M[_], A](f: Depth => List[A]): Series[M, A] =
+    Series[M, A](
+      Kleisli[({type l[a] = LogicT[M, a]})#l, Depth, A]{ d =>
+        val M = LogicT.logicTMonadPlus[M]
+        f(d).map(
+          LogicT.logicTMonadPlus[M].point(_)
+        ).foldLeft(M.empty[A])(M.plus(_, _))
+      }
+    )
 
 }
